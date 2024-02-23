30848a4277102f5f3731af8d9b667244933fd338
Baptiste <batbox.longuepee@gmail.com>
2024-02-23 11:17:01 +0100

    Fix(error 404)

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,26 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import {PrismaModule} from "./prisma/prisma.module";
+import {AppController} from "./app.controller";
+import {AppService} from "./app.service";
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-    }),
-    TypeOrmModule.forRoot({
-      type: process.env.DB_TYPE as any,
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-      entities: ['dist/**/*.entity.js'],
-      synchronize: true,
-    }),
-    UsersModule,
-    AuthModule,
-  ],
-  controllers: [],
-  providers: [],
+  imports: [PrismaModule, AuthModule, ConfigModule.forRoot()],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
